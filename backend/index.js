9a44ecdda9f3ec2ebc44308230a21bb3121b59ac
abzak <abzakcd@gmail.com>
2025-03-13 19:30:38 +0200

    base node js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
-
-
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 const app = express();
+const HTTP_PORT = 3004;
 
-const PORT = 3000;
+dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express!');
-});
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+
+
+app.listen(HTTP_PORT, () => {
+  console.log(`Server running on http://localhost:${HTTP_PORT}`);
 });
