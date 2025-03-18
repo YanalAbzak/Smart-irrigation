121d5fd5496d8438b5439966c61fef031581a874
abzak <abzakcd@gmail.com>
2025-03-18 02:52:05 +0200

    update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-// Create a new Sequelize instance for database connection
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -9,7 +8,7 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: "mysql",
-        logging: false, // Disable logging SQL queries in console
+        logging: false,
     }
 );
 
